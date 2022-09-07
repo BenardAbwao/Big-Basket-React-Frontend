e133f959cf612ba53bfa014173db58329f77f7f1
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-09-07 23:57:31 +0300

    create navigation

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+
+  return (
+    <div className="Navbar">
+      <span className="nav-logo">
+        <h1>Big Basket</h1>
+        <h2> Inspire Yourself With These Wonderful Sentiments</h2>
+      </span>
+    </div>
+  );
+}
+
+export default Navbar
