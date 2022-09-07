47dec014241d952d04d6d2582875a6a48aee68e9
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-09-08 00:05:56 +0300

    set quote update button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React,{ useState, useEffect } from 'react';
+import axios from "axios"
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+const App = () => {
+  const [quotes, setQuotes] = useState("");
+  const [author, setAuthor] = useState("");
+ 
+   const API = async () => {
+    let quotesarr = [];
+    try{
+      const data = await axios.get("http://localhost:9292/quotes");
+      quotesarr = data.data;
+    
+    
+    }catch(error){
+      console.log(error)
+    }
+    try {
+     setQuotes(quotesarr.content);
+     setAuthor(quotesarr.author);
+    }catch (error){
+      console.log(error)
+    }
+  };
+    useEffect(() => {
+      API();
+    }, []);
+    function handleUpdateQuote(updatedQuote) {
+      const updatedQuotes = quotes.map((quote) => {
+        if (quote.id === updatedQuote.id) {
+          return updatedQuote;
+        } else {
+          return quote;
+        }
+      });
+      setQuotes(updatedQuotes);
+    }
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    <Navbar/>
+      <div className="App">
+        <div className="quotebox">
+          <div className="container">
+            <div className="quote">
+              <h2>{quotes}</h2>
+            </div>
+            <div className="quotebutton">
+              <button onClick={API}>Generate Quotes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Home onUpdateQuotes={handleUpdateQuote}/>
+    </>
   );
 }
 
