3e99e2cb31b1fedc5416561a5befbf9f4a6bbf5e
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-09-09 08:29:57 +0300

    update nav1 text

--- src/components/Nav1.js
+++ src/components/Nav1.js
@@ -4,7 +4,7 @@ function Nav1() {
   return (
     <div className="Nav">
       <span className="nav">
-        <h1>ADD YOUR FAVORITE QUOTE</h1>
+        <h1>ADD YOUR FAVORITE INSPIRATION</h1>
       </span>
     </div>
   );
